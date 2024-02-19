cd5eaea707b96f436a48f411021a545443e0084c
MChikani <moh.kareem15@gmail.com>
2024-02-19 13:41:51 -0500

    DAL updates

--- dal/main.go
+++ dal/main.go
@@ -5,6 +5,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"go.mongodb.org/mongo-driver/bson"
 	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,15 +15,15 @@ import (
 // Define a struct to represent your data model
 type User struct {
 	ID       string `bson:"_id,omitempty"`
-	Username string `bson:"username"`
+	Name     string `bson:"name"`
+	Password string `bson:"password"`
 	Email    string `bson:"email"`
 }
 
 // MongoDB configuration
 var (
-	mongoURI       = "mongodb://localhost:27017" // MongoDB server URI
-	dbName         = "mydb"                      // Database name
-	collectionName = "users"                     // Collection name
+	mongoURI = "mongodb://localhost:27017" // MongoDB server URI
+	dbName   = "smartHomeDB"               // Collection name
 )
 
 // Connect to MongoDB and return a MongoDB client
@@ -45,8 +46,25 @@ func connectToMongoDB() (*mongo.Client, error) {
 
 // Create a new user in the MongoDB database
 func createUser(client *mongo.Client, user User) error {
-	collection := client.Database(dbName).Collection(collectionName)
+	collection := client.Database(dbName).Collection("users")
 	_, err := collection.InsertOne(context.Background(), user)
+	return err
+}
+
+func fetchUser(client *mongo.Client, key, value string) (User, error) {
+	collection := client.Database(dbName).Collection("users")
+	filter := bson.M{key: value}
+
+	var user User
+	err := collection.FindOne(context.Background(), filter).Decode(&user)
+	return user, err
+}
+
+func deleteUser(client *mongo.Client, key, value string) error {
+	collection := client.Database(dbName).Collection("users")
+	filter := bson.M{key: value}
+
+	_, err := collection.DeleteOne(context.Background(), filter)
 	return err
 }
 
@@ -60,7 +78,8 @@ func main() {
 
 	// Example: Creating a new user
 	newUser := User{
-		Username: "john_doe",
+		Name:     "john_doess",
+		Password: "passlel",
 		Email:    "john@example.com",
 	}
 
@@ -70,4 +89,12 @@ func main() {
 	}
 
 	fmt.Println("User created successfully!")
+
+	// Example: Fetching a user by username
+	fetchedUser, err := fetchUser(client, "name", "john_doess")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(fetchedUser)
+	fmt.Println(fetchedUser.Name)
 }

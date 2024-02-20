4edd92500c98f81e46fc16f10593cee31e020ddc
CBussom <cameron.bussom@gmail.com>
2024-02-19 19:06:13 -0500

    working on ticket to test out appliances trying to get the user input string to be sent as the message for testing purposes

--- main.go
+++ main.go
@@ -8,19 +8,18 @@ import (
 // Git not working big annoying
 func main() {
 	// Create a new light switch appliance
-	lightSwitch := appliances.NewAppliance("Light Switch", false, 0)
+	//lightSwitch := appliances.NewAppliance("Light Switch", false, 0)
 	ReceivedVal := appliances.ReceiverController()
 	fmt.Println("Value is: ", ReceivedVal)
-	if ReceivedVal == "lights off" {
-		lightSwitch.TurnOff()
-	}
-
-	if ReceivedVal == "lights on" {
-		lightSwitch.TurnOn()
-	}
+	//if ReceivedVal == "lights off" {
+	//	lightSwitch.TurnOff()
+	//}
+	//
+	//if ReceivedVal == "lights on" {
+	//	lightSwitch.TurnOn()
+	//}
 
 	// Use the appliance
-
 	oven := appliances.NewAppliance("Oven", false, 0)
 
 	oven.AdjustTemp(300)

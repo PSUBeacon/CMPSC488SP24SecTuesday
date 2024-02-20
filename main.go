d76b62c7312187dd05b45a62c6ea6416217c77cc
CBussom <cameron.bussom@gmail.com>
2024-02-19 20:01:20 -0500

    working on ticket to test out appliances trying to get the user input string changed the inputs to add a temp change and some other stuff

--- main.go
+++ main.go
@@ -8,19 +8,22 @@ import (
 // Git not working big annoying
 func main() {
 	// Create a new light switch appliance
-	lightSwitch := appliances.NewAppliance("Light Switch", false, 0)
+	//lightSwitch :=
+	appliances.NewAppliance("Light Switch", false, 0)
+	oven := appliances.NewAppliance("Oven", false, 0)
 	ReceivedVal := appliances.ReceiverController()
 	fmt.Println("Value is: ", ReceivedVal)
-	if ReceivedVal == "lights off" {
-		lightSwitch.TurnOff()
+	if ReceivedVal == "oven off" {
+		oven.TurnOff()
 	}
 
-	if ReceivedVal == "lights on" {
-		lightSwitch.TurnOn()
+	if ReceivedVal == "oven on" {
+		oven.TurnOn()
+	}
+
+	if ReceivedVal == "temp change" {
+		oven.AdjustTemp(300)
 	}
 
 	// Use the appliance
-	oven := appliances.NewAppliance("Oven", false, 0)
-	oven.AdjustTemp(300)
-	oven.TurnOn()
 }

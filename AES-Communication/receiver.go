2bedf182b7ab09d659f40beae222157e24e3ef97
CBussom <cameron.bussom@gmail.com>
2024-02-21 17:52:16 -0500

    Trying to get the block to be stored in a json file

--- AES-Communication/receiver.go
+++ AES-Communication/receiver.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"crypto/aes"
 	"crypto/cipher"
+	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
 	"go.bug.st/serial"
@@ -63,7 +64,7 @@ func decryptAES(key, ciphertext []byte) ([]byte, error) {
 
 func ConfigureController() {
 	// Open the XBee module for communication
-	//var block []Block
+	var block []Block
 	mode := &serial.Mode{
 		BaudRate: 9600,
 	}
@@ -114,18 +115,17 @@ func ConfigureController() {
 		}
 		fmt.Printf("Decrypted text: %s\n", decryptedText)
 
-		//tojson := json.Unmarshal(decryptedText, &block)
-		//if tojson != nil {
-		//
-		//	// if error is not nil
-		//	// print error
-		//	fmt.Println(tojson)
-		//}
-		////fmt.Println(tojson)
-		//for i := range block {
-		//	fmt.Println(string(rune(block[i].Index)) + " - " + block[i].Timestamp +
-		//		" - " + block[i].Data + " - " + block[i].PrevHash + " - " + block[i].Hash)
-		//}
+		tojson := json.Unmarshal(decryptedText, &block)
+		if tojson != nil {
+
+			// if error is not nil
+			// print error
+			fmt.Println(tojson)
+		}
+		//fmt.Println(tojson)
+		for i := range block {
+			fmt.Println(string(rune(block[i].Index)) + " - " + block[i].Timestamp + " - " + block[i].Data + " - " + block[i].PrevHash + " - " + block[i].Hash)
+		}
 	}
 }
 

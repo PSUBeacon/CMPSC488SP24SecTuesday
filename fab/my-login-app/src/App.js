4c0f0e3c32f4f7e017d838dfd517574b613e8eeb
TreasureAD <treasuredavis111@gmail.com>
2024-02-21 20:33:04 -0500

    Update App.js

--- fab/my-login-app/src/App.js
+++ fab/my-login-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginForm from './LoginForm'; // Make sure the path matches where you saved LoginForm.js
 import './login.css';
+import './Dashboard.css';
 import Dashboard from './Dashboard'; // Import Dashboard component
 
 function App() {

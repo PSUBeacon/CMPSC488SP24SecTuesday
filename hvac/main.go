cc09bdf115d3a4d5738df5d824ed4a2b744fb7c9
MarkV203 <muv67@psu.edu>
2024-02-21 21:10:52 -0500

    Added vars to match db

--- hvac/main.go
+++ hvac/main.go
@@ -6,23 +6,27 @@ import (
 
 // HVAC represents an HVAC system with temperature control, fan speed, and mode.
 type HVAC struct {
-	Name        string
-	Temperature int // Desired temperature in Celsius
-	FanSpeed    int // Fan speed (0-100%)
-	Humidity    int
-	Mode        string // HVAC mode (e.g., "Cool", "Heat", "Fan", "Off")
-	Zone        int
+	Name              string
+	Temperature       int    // Desired temperature in Celsius
+	FanSpeed          string // Fan speed ("Off" , "Low", "Medium", "High")
+	Humidity          int    // Humidity %
+	Status            string // HVAC mode (e.g., "Cool", "Heat", "Fan", "Off")
+	Location          string // Location of device
+	EnergyConsumption int
+	LastChanged       string
 }
 
 // NewHVAC creates a new HVAC instance with the given name and initial settings.
 func NewHVAC(name string) *HVAC {
 	return &HVAC{
-		Name:        name,
-		Temperature: 25, // Initial temperature setting
-		Humidity:    80,
-		FanSpeed:    50,    // Initial fan speed setting (50%)
-		Mode:        "Off", // Initial mode is Off
-		Zone:        1,
+		Name:              "Humidity Sensor",
+		Temperature:       25, // Initial temperature setting
+		Humidity:          80,
+		FanSpeed:          "Low", // Initial fan speed setting (50%)
+		Status:            "Off", // Initial mode is Off
+		Location:          "Kitchen",
+		EnergyConsumption: 10,
+		LastChanged:       "2023-10-01T18:30:00Z",
 	}
 }
 
@@ -33,20 +37,15 @@ func (h *HVAC) SetTemperature(temperature int) {
 }
 
 // SetFanSpeed sets the fan speed for the HVAC system.
-func (h *HVAC) SetFanSpeed(speed int) {
-	if speed < 0 {
-		speed = 0
-	} else if speed > 100 {
-		speed = 100
-	}
+func (h *HVAC) SetFanSpeed(speed string) {
 	h.FanSpeed = speed
 	fmt.Printf("%s fan speed is set to %d%%\n", h.Name, h.FanSpeed)
 }
 
-// SetMode sets the mode (e.g., "Cool", "Heat", "Fan", "Off") for the HVAC system.
-func (h *HVAC) SetMode(mode string) {
-	h.Mode = mode
-	fmt.Printf("%s mode is set to %s\n", h.Name, h.Mode)
+// SetMode sets the status (e.g., "Cool", "Heat", "Fan", "Off") for the HVAC system.
+func (h *HVAC) SetStatus(status string) {
+	h.Status = status
+	fmt.Printf("%s mode is set to %s\n", h.Name, h.Status)
 }
 
 func (h *HVAC) SetHumidity(humidity int) {

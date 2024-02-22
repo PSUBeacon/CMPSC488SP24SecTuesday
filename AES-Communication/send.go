204a871520f55780a71f9f8cb3e0cafde2c2a43a
CBussom <52250345+CBussom@users.noreply.github.com>
2024-02-22 09:43:09 -0500

    added a different delim

--- AES-Communication/send.go
+++ AES-Communication/send.go
@@ -77,7 +77,7 @@ func SendMessagesToServer() {
 	//sender := xbee.NewSender(port)
 	// Configure XBee module as a client
 	//sendmessage := encryptedBlock
-	sendmessage := append(encryptedBlock, '?')
+	sendmessage := append(encryptedBlock, '☺')
 	for {
 		// Send a message to the server
 		fmt.Println(len(sendmessage))

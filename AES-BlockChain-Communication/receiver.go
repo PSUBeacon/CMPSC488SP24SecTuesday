a586fb533ad9fa9aca051dad92985392f7a67d14
CBussom <cameron.bussom@gmail.com>
2024-02-22 17:12:56 -0500

    moved around functions and added different cases to see if it will help

--- AES-BlockChain-Communication/receiver.go
+++ AES-BlockChain-Communication/receiver.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"bufio"
-	"crypto/aes"
-	"crypto/cipher"
 	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
@@ -22,47 +20,6 @@ type Block struct {
 
 type Blockchain struct {
 	Chain []Block
-}
-
-func decryptAES(key, ciphertext []byte) ([]byte, error) {
-	//ciphertext = []byte(strings.Trim(string(ciphertext), "*"))
-	block, err := aes.NewCipher(key)
-
-	if err != nil {
-		return nil, err
-	}
-
-	if len(ciphertext) < aes.BlockSize {
-		return nil, fmt.Errorf("ciphertext too short")
-	}
-
-	//fmt.Printf("Ciphertext length (including IV): %d\n", len(ciphertext))
-
-	iv := ciphertext[:aes.BlockSize]
-	ciphertext = ciphertext[aes.BlockSize:]
-
-	//fmt.Printf("Ciphertext length (excluding IV): %d\n", len(ciphertext))
-
-	if len(ciphertext)%aes.BlockSize != 0 {
-		return nil, fmt.Errorf("ciphertext is not a multiple of the block size")
-	}
-
-	mode := cipher.NewCBCDecrypter(block, iv)
-	mode.CryptBlocks(ciphertext, ciphertext)
-
-	// Remove padding
-	padding := int(ciphertext[len(ciphertext)-1])
-	if padding < 1 || padding > aes.BlockSize {
-		return nil, fmt.Errorf("invalid padding")
-	}
-	for i := len(ciphertext) - padding; i < len(ciphertext); i++ {
-		if ciphertext[i] != byte(padding) {
-			return nil, fmt.Errorf("invalid padding")
-		}
-	}
-	ciphertext = ciphertext[:len(ciphertext)-padding]
-
-	return ciphertext, nil
 }
 
 func blockReceiver() {
@@ -124,7 +81,7 @@ func blockReceiver() {
 			fmt.Println(tojson)
 		}
 
-		//fmt.Println(tojson)
+		fmt.Println(chain)
 		//for i := range chain {
 		//	fmt.Println(string(rune(block[i].Index)) + " - " + block[i].Timestamp + " - " + block[i].Data + " - " + block[i].PrevHash + " - " + block[i].Hash)
 		//}
--- AES-BlockChain-Communication/send.go
+++ AES-BlockChain-Communication/send.go
@@ -2,44 +2,14 @@ package main
 
 import (
 	"CMPSC488SP24SecTuesday/blockchain"
-	"bytes"
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
 	"go.bug.st/serial"
-	"io"
 	"log"
 	"os"
 	"time"
 )
-
-func encryptAES(key, plaintext []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-
-	// PKCS#7 padding
-	padding := block.BlockSize() - len(plaintext)%block.BlockSize()
-	padText := bytes.Repeat([]byte{byte(padding)}, padding)
-	plaintext = append(plaintext, padText...)
-
-	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
-	iv := ciphertext[:aes.BlockSize]
-	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
-		return nil, err
-	}
-
-	mode := cipher.NewCBCEncrypter(block, iv)
-	mode.CryptBlocks(ciphertext[aes.BlockSize:], plaintext)
-
-	fmt.Printf("Ciphertext length after encryption: %d\n", len(ciphertext))
-
-	return ciphertext, nil
-}
 
 func SendMessagesToServer() {
 	// The key should be 16, 24, or 32 bytes long for AES-128, AES-192, or AES-256, respectively.

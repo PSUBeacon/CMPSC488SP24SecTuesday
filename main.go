fc99a6ce8813669359d694696d2f3571d1570fc1
Charles Langley <79470963+DaboiCharles@users.noreply.github.com>
2024-02-21 23:28:29 -0500

    Update main.go
    created a if statements to test security methods for reviever and client

--- main.go
+++ main.go
@@ -1,29 +1,69 @@
 package main
 
 import (
-	"CMPSC488SP24SecTuesday/appliances"
 	"fmt"
 )
 
 // Git not working big annoying
+// I agree @Cameron
 func main() {
-	// Create a new light switch appliance
-	//lightSwitch :=
-	appliances.NewAppliance("Light Switch", false, 0)
-	oven := appliances.NewAppliance("Oven", false, 0)
+	// Creates alarm, padlock, front door, back door
+
+	securityAlarm := NewAlarm("Security Alarm")
+	padLock1 := NewPadlock("PadLock1", "1234")
+	frontdoor := NewDoorLock("FrontDoor", true)
+	backdoor := NewDoorLock("BackDoor", true)
 	ReceivedVal := appliances.ReceiverController()
 	fmt.Println("Value is: ", ReceivedVal)
-	if ReceivedVal == "oven off" {
-		oven.TurnOff()
+
+	if ReceivedVal == "lock front door" {
+		frontdoor.LockOrUnlock(true)
 	}
 
-	if ReceivedVal == "oven on" {
-		oven.TurnOn()
+	if ReceivedVal == "unlock front door" {
+		frontdoor.LockOrUnlock(false)
 	}
 
-	if ReceivedVal == "temp change" {
-		oven.AdjustTemp(300)
+	if ReceivedVal == "unlock back door" {
+		backdoor.LockOrUnlock(false)
 	}
 
-	// Use the appliance
+	if ReceivedVal == "lock back door" {
+		backdoor.LockOrUnlock(true)
+	}
+
+	if ReceivedVal == "lock back door" {
+		backdoor.LockOrUnlock(true)
+	}
+
+	if ReceivedVal == "4321" {
+
+		padLock1.Verify("4321")
+	}
+
+	if ReceivedVal == "arm" {
+
+		securityAlarm.Arm()
+	}
+	if ReceivedVal == "disarm" {
+
+		securityAlarm.Disarm()
+	}
+
+	if ReceivedVal == "trigger" {
+
+		securityAlarm.Trigger()
+	}
+
+	if ReceivedVal == "detect" {
+
+		motionSensor.DetectMotion()
+	}
+
+	if ReceivedVal == "detect" {
+
+		motionSensor.DetectMotion()
+	}
+
+	// Use security modules
 }

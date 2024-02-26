6549050ecb883fe777e6cf7ee4b87346c63c4560
CBussom <cameron.bussom@gmail.com>
2024-02-25 22:26:19 -0500

    everything broke the sending, fixing it now

--- AES-BlockChain-Communication/send.go
+++ AES-BlockChain-Communication/send.go
@@ -40,9 +40,26 @@ func encryptAES(key, plaintext []byte) ([]byte, error) {
 	return ciphertext, nil
 }
 
-func broadCastMessage(messageToSend string) {
+func broadCastMessage(messageToSend []byte) {
 	// The key should be 16, 24, or 32 bytes long for AES-128, AES-192, or AES-256, respectively.
-	err := godotenv.Load()
+
+	// Open the XBee module for communication
+	mode := &serial.Mode{
+		BaudRate: 9600,
+	}
+	//fmt.Println("This is in the send function", message)
+	port, err := serial.Open("/dev/ttyUSB0", mode)
+	if err != nil {
+		log.Fatal("Error opening XBee module:", err)
+	}
+	defer func(port serial.Port) {
+		err := port.Close()
+		if err != nil {
+
+		}
+	}(port)
+
+	err = godotenv.Load()
 	AesKey := os.Getenv("AES_KEY")
 
 	jsonChainData, err := os.ReadFile("chain.json")
@@ -55,7 +72,7 @@ func broadCastMessage(messageToSend string) {
 	//Checks if there is an existing chain or if this is the start of the chain
 	if chainlen == 0 {
 		chain := blockchain.NewBlockchain()
-		block := blockchain.CreateBlock(messageToSend)
+		block := blockchain.CreateBlock(string(messageToSend))
 		chain.Chain = append(chain.Chain, block)
 
 		fmt.Println("This is the chain: ", chain)
@@ -75,59 +92,43 @@ func broadCastMessage(messageToSend string) {
 			log.Fatal("Error encrypting block:", err)
 		}
 
-		send(encryptedBlockChain)
+		fmt.Println("This is before delim added", encryptedBlockChain)
+		delimiter := []byte{0xE2, 0x99, 0xB4}
+		encryptedBlockChain = append(delimiter)
+		fmt.Println("This is after delim is added", encryptedBlockChain)
+
+		// Send a message to the server
+		fmt.Println(len(encryptedBlockChain))
+		_, err = port.Write(encryptedBlockChain)
+		fmt.Printf("Sent \n")
+		if err != nil {
+			log.Println("Error sending message:", err)
+		}
 		return
 
 	}
-	if chainlen > 0 {
-		block := blockchain.CreateBlock(messageToSend)
-		jsonBlock, err := json.Marshal(block)
-		err = os.WriteFile("chain.json", jsonBlock, 0644)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("This is the block before encryption: ", jsonBlock)
-		encryptedBlock, err := encryptAES([]byte(AesKey), jsonBlock)
-		if err != nil {
-			log.Fatal("Error encrypting block:", err)
-		}
+	/*
+		if chainlen > 0 {
+			block := blockchain.CreateBlock(messageToSend)
+			jsonBlock, err := json.Marshal(block)
+			err = os.WriteFile("chain.json", jsonBlock, 0644)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("This is the block before encryption: ", jsonBlock)
+			encryptedBlock, err := encryptAES([]byte(AesKey), jsonBlock)
+			if err != nil {
+				log.Fatal("Error encrypting block:", err)
+			}
 
-		send(encryptedBlock)
-		return
-	}
-}
-func send(message []byte) {
-	// Open the XBee module for communication
-	mode := &serial.Mode{
-		BaudRate: 9600,
-	}
-	//fmt.Println("This is in the send function", message)
-	port, err := serial.Open("/dev/ttyUSB0", mode)
-	if err != nil {
-		log.Fatal("Error opening XBee module:", err)
-	}
-	defer func(port serial.Port) {
-		err := port.Close()
-		if err != nil {
-
+			send(encryptedBlock)
+			return
 		}
-	}(port)
-	fmt.Println("This is before delim added", message)
-	delimiter := []byte{0xE2, 0x99, 0xB4}
-	message = append(delimiter)
-	fmt.Println("This is after delim is added", message)
-	// Send a message to the server
-	fmt.Println(len(message))
-	_, err = port.Write(message)
-	fmt.Printf("Sent \n")
-	if err != nil {
-		log.Println("Error sending message:", err)
-	}
-	//time.Sleep(5 * time.Second) // Send message every 5 seconds
+	*/
 }
 
 func main() {
 
-	broadCastMessage("testing the stuff")
+	broadCastMessage([]byte("testing the stuff"))
 
 }

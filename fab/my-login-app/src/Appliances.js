96b2f8d23c0f9411e68cd1038118592ef79b5eb0
mabraham2o24 <mahima200016@gmail.com>
2024-03-05 10:50:08 -0500

    pages
    created simple pages for all the side navigation menu options.

---
+++ fab/my-login-app/src/Appliances.js
@@ -0,0 +1,16 @@
+// Appliances.js
+
+import React from 'react';
+import './Appliances.css'; // Import CSS file
+
+const Security = () => {
+  return (
+    // Use the class name that matches your CSS file
+    <div className="appliancesPage">
+      <h2>Appliance Page</h2>
+      <p>Contents of the Appliance Page</p>
+    </div>
+  );
+};
+
+export default Security;
---
+++ fab/my-login-app/src/Lighting.js
@@ -0,0 +1,16 @@
+// Lighting.js
+
+import React from 'react';
+import './Lighting.css'; // Import CSS file
+
+const Security = () => {
+  return (
+    // Use the class name that matches your CSS file
+    <div className="lightingPage">
+      <h2>Lighting Page</h2>
+      <p>Contents of the Lighting Page</p>
+    </div>
+  );
+};
+
+export default Security;

c2486736b8d1ebf0543a6b49c177a1b028833deb
Charles Langley <79470963+DaboiCharles@users.noreply.github.com>
2024-03-12 16:37:41 -0400

    Update main.go Appliances
    added serialization and deserialization

--- appliances/main.go
+++ appliances/main.go
@@ -1,14 +1,15 @@
-package appliances
+package main
 
 import (
+	"encoding/json"
 	"fmt"
 )
 
 // Appliance represents a simple appliance with an on/off state.
 type Appliance struct {
-	Name  string
-	State bool
-	Temp  int
+	Name  string `json:"Name"`
+	State bool   `json:"State"`
+	Temp  int    `json:"Temp"`
 }
 
 // NewAppliance creates a new Appliance instance with the given name and initial state.
@@ -35,4 +36,52 @@ func (a *Appliance) TurnOff() {
 func (a *Appliance) AdjustTemp(setTemp int) {
 	a.Temp = setTemp
 	fmt.Printf("%s temperature is now set to %d degrees farenheit\n", a.Name, a.Temp)
+}
+func serializeAppliance(appliance *Appliance) (string, error) {
+	applianceJSON, err := json.MarshalIndent(appliance, "", " ")
+	if err != nil {
+		return "", err
+	}
+	return string(applianceJSON), nil
+}
+
+func deserializeAppliance(applianceJSON string) (*Appliance, error) {
+	var appliance Appliance
+	err := json.Unmarshal([]byte(applianceJSON), &appliance)
+	if err != nil {
+		return nil, err
+	}
+	return &appliance, nil
+}
+
+func main() {
+	myAppliance := NewAppliance("Toaster", false, 85)
+
+	//on
+	myAppliance.TurnOn()
+
+	//off
+	myAppliance.TurnOff()
+
+	//set temp
+	myAppliance.AdjustTemp(100)
+
+	// Serialize the Appliance instance to JSON
+	applianceJSON, err := serializeAppliance(myAppliance)
+	if err != nil {
+		fmt.Println("Error serializing APPLIANCES to JSON:", err)
+		return
+	}
+
+	// Print the serialized JSON string
+	fmt.Println("\nSerialized appliance: \n" + applianceJSON)
+
+	// Deserialize the JSON back into a Appliance object
+	deserializeAppliance, err := deserializeAppliance(applianceJSON)
+	if err != nil {
+		fmt.Println("Error deserializing appliance:", err)
+		return
+	}
+
+	fmt.Printf("\nDeserialized Appliance:\n %+v\n", deserializeAppliance)
 }

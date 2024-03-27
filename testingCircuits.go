bccec603fc567b1095d0e3cb0f7f5beb444a9909
CBussom <cameron.bussom@gmail.com>
2024-03-26 20:55:17 -0400

    test circuit

--- testingCircuits.go
+++ testingCircuits.go
@@ -3,7 +3,6 @@ package main
 import "CMPSC488SP24SecTuesday/on-metal-c-code/gocode"
 
 func main() {
-	gocode.Switchable(7, true)
-	gocode.Switchable(0, true)
-	gocode.Switchable(4, true)
+	gocode.Switchable(17, true)
+
 }

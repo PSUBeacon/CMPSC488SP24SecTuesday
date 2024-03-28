ac4b8927fc39e79a3abf5b10628460114266098d
CBussom <52250345+CBussom@users.noreply.github.com>
2024-03-28 11:45:51 -0400

    testing stuff

---
+++ testingMotion.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+// CheckForMotion initializes the GPIO then checks for motion
+func CheckForMotion(pinNum uint8) (bool, error) {
+	if err := rpio.Open(); err != nil {
+		return false, err
+	}
+	defer func() {
+		err := rpio.Close()
+		if err != nil {
+
+		}
+	}()
+
+	pin := rpio.Pin(pinNum)
+	pin.Input()
+	pin.PullDown()
+
+	motionDetected := pin.Read() == rpio.High
+	return motionDetected, nil
+}
+
+func main() {
+	for true {
+		motion, _ := CheckForMotion(11)
+		fmt.Println("the motion is ", motion)
+	}
+}

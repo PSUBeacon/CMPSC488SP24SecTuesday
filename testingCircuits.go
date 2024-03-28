b423832f10e65b77676b5fda127cfd9cd67ba0b8
CBussom <cameron.bussom@gmail.com>
2024-03-28 00:12:50 -0400

    testing matrix

--- testingCircuits.go
+++ testingCircuits.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"github.com/d2r2/go-max7219"
+	"github.com/stianeikeland/go-rpio/v4"
 	"log"
+	"time"
 )
 
 //
@@ -29,24 +30,46 @@ import (
 //}
 
 func main() {
-	// Create a new device with 1 cascaded MAX7219
-	device := max7219.NewDevice(1)
-
-	// Open the device (using SPI bus 0, device 0)
-	if err := device.Open(0, 0, 15); err != nil {
-		log.Fatal(err)
+	// Open/initialize the RPi's GPIO memory range for use
+	err := rpio.Open()
+	if err != nil {
+		log.Fatalf("Error initializing GPIO: %v", err)
 	}
-	defer device.Close()
+	defer rpio.Close()
 
-	// Set all LEDs to on
-	for i := 0; i < 8; i++ {
-		if err := device.SetBufferLine(0, i, 0xFF, true); err != nil {
-			log.Fatal(err)
-		}
+	// Set up pin 4 as output (Chip Select)
+	csPin := rpio.Pin(4)
+	csPin.Output()
+	csPin.High() // Set pin high (inactive) initially
+
+	// Set up SPI communication
+	err = rpio.SpiBegin(rpio.Spi0)
+	if err != nil {
+		log.Fatalf("Error initializing SPI: %v", err)
+	}
+	defer rpio.SpiEnd(rpio.Spi0)
+
+	rpio.SpiSpeed(1000000) // Set SPI speed to 1MHz (adjust as needed)
+	rpio.SpiChipSelect(0)  // Use CS0 (this will be overridden by manual control of the CS pin)
+	rpio.SpiMode(0, 0)     // Set SPI mode (adjust as needed)
+
+	// Map physical pins to SPI function
+	rpio.Pin(9).Mode(rpio.Alt0)  // Set pin 9 as SPI0 MOSI (DIN)
+	rpio.Pin(10).Mode(rpio.Alt0) // Set pin 10 as SPI0 SCLK (CLK)
+
+	// Prepare data to turn on all LEDs in the matrix
+	// This data format will depend on the specific LED matrix you're using
+	// For this example, we're sending 8 bytes, each with all bits set to 1 (0xFF)
+	data := make([]byte, 8)
+	for i := range data {
+		data[i] = 0xFF
 	}
 
-	// Update the display
-	if err := device.Flush(); err != nil {
-		log.Fatal(err)
-	}
+	// Send data to turn on all LEDs
+	csPin.Low()               // Activate the CS line
+	rpio.SpiTransmit(data...) // Send the data
+	csPin.High()              // Deactivate the CS line
+
+	// Keep the LEDs on for a while to observe
+	time.Sleep(5 * time.Second)
 }

12b9e70dba3340c23453a8ef37a700814f8f13b6
CBussom <52250345+CBussom@users.noreply.github.com>
2024-03-28 14:53:35 -0400

    thats a wrap maybe

--- maybe.py
+++ maybe.py
@@ -1,15 +1,63 @@
-from luma.led_matrix.device import max7219
-from luma.core.interface.serial import spi, noop
-from luma.core.render import canvas
+from gpiozero import DigitalOutputDevice
+from time import sleep
 
-# Create a serial interface with the correct GPIO pin numbers
-serial = spi(port=0, device=0, gpio=noop())
+# Define the GPIO pins
+CS_PIN = 4
+DIN_PIN = 9
+CLK_PIN = 10
 
-# Create a device representing the LED matrix
-device = max7219(serial, cascaded=1, block_orientation=90, rotate=0)
+# Create DigitalOutputDevice objects for each pin
+cs = DigitalOutputDevice(CS_PIN)
+din = DigitalOutputDevice(DIN_PIN)
+clk = DigitalOutputDevice(CLK_PIN)
 
-# Turn on all the LEDs in the matrix
-with canvas(device) as draw:
-    draw.rectangle(device.bounding_box, outline="white", fill="white")
+def send_byte(data):
+    for i in range(8):
+        # Set DIN to the value of the most significant bit
+        din.value = (data & 0x80) != 0
+        # Pulse the clock
+        clk.on()
+        sleep(0.0001)
+        clk.off()
+        # Shift the data left by one bit
+        data <<= 1
 
-# The LEDs will stay on until the program is terminated
+def send_command(register, data):
+    # Pull CS low to select the device
+    cs.off()
+    # Send the register address and data
+    send_byte(register)
+    send_byte(data)
+    # Pull CS high to release the device
+    cs.on()
+
+def initialize_matrix():
+    # Set the number of digits to 8
+    send_command(0x0B, 0x07)
+    # Set the intensity (brightness) to a medium value
+    send_command(0x0A, 0x04)
+    # Exit shutdown mode
+    send_command(0x0C, 0x01)
+    # Disable display test
+    send_command(0x0F, 0x00)
+
+def clear_matrix():
+    for i in range(1, 9):
+        send_command(i, 0x00)
+
+def fill_matrix():
+    for i in range(1, 9):
+        send_command(i, 0xFF)
+
+# Initialize and clear the matrix
+initialize_matrix()
+clear_matrix()
+
+# Fill the matrix
+fill_matrix()
+
+# Wait for 5 seconds
+sleep(5)
+
+# Clear the matrix
+clear_matrix()

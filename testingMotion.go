8841a8adb8d695283805a17e76cd261e0a2839ba
CBussom <52250345+CBussom@users.noreply.github.com>
2024-03-28 12:50:30 -0400

    testing

--- testingMotion.go
+++ testingMotion.go
@@ -2,39 +2,37 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
-// CheckForMotion initializes the GPIO then checks for motion
-func CheckForMotion(pinNum uint8) (bool, error) {
-	if err := rpio.Open(); err != nil {
-		return false, err
-	}
-	defer func() {
-		err := rpio.Close()
-		if err != nil {
-
-		}
-	}()
-
-	pin := rpio.Pin(pinNum)
-	pin.Input()
-	pin.PullUp() // Changed to pull-up
-
-	motionDetected := pin.Read() == rpio.Low // Changed to detect low signal
-	return motionDetected, nil
-}
+const sensorPin = 11
 
 func main() {
+	// Open and map memory to access gpio, check for errors
+	if err := rpio.Open(); err != nil {
+		fmt.Println("Unable to open GPIO:", err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	// Set pin to input mode
+	pin := rpio.Pin(sensorPin)
+	pin.Input()
+
+	fmt.Println("Monitoring for motion...")
+
 	for {
-		motion, err := CheckForMotion(11)
-		if err != nil {
-			fmt.Printf("Error checking for motion: %v\n", err)
-			return
+		// Read pin state
+		if pin.Read() == rpio.High {
+			fmt.Println("Motion detected!")
+		} else {
+			fmt.Println("No motion.")
 		}
-		fmt.Printf("Motion detected: %t\n", motion)
+
+		// Sleep for a while to reduce CPU usage
 		time.Sleep(1 * time.Second)
 	}
 }

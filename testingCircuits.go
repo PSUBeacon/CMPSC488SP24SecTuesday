73c0e4588464fc50be1d4fdf834610d1e4be093e
CBussom <cameron.bussom@gmail.com>
2024-03-27 21:46:54 -0400

    testing lcd

--- testingCircuits.go
+++ testingCircuits.go
@@ -1,4 +1,7 @@
-//package main
+package main
+
+import "CMPSC488SP24SecTuesday/on-metal-c-code/gocode"
+
 //
 //import (
 //	"flag"
@@ -22,39 +25,7 @@
 //
 //}
 
-package main
-
-import (
-	lcd "github.com/wjessop/lcm1602_lcd"
-	"golang.org/x/exp/io/i2c"
-	"log"
-)
-
 func main() {
-	// Configure this line with the device location and address of your device
-	lcdDevice, err := i2c.Open(&i2c.Devfs{Dev: "/dev/i2c-1"}, 0x27)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer lcdDevice.Close()
-
-	lcdDisplay, err := lcd.NewLCM1602LCD(lcdDevice)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Write a string to row 1, position 0 (ie, the start of the line)
-	err = lcdDisplay.WriteString("Hello World!", 1, 0)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Write a string to row 2, position 7
-	if err := lcdDisplay.WriteString("(>'.'<)", 2, 7); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := lcdDisplay.Clear(); err != nil {
-		log.Fatal(err)
-	}
+	gocode.ClearLCD()
+	gocode.WriteLCD("This worked")
 }

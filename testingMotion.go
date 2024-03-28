3b91488351c12739e66ae99352e830cc5e534c2f
CBussom <52250345+CBussom@users.noreply.github.com>
2024-03-28 12:54:18 -0400

    testing

--- testingMotion.go
+++ testingMotion.go
@@ -3,36 +3,49 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
-const sensorPin = 11
+// PIR struct represents a PIR motion sensor
+type PIR struct {
+	signalPin rpio.Pin
+}
+
+// NewPIR creates a new PIR instance
+func NewPIR(pinNumber int) *PIR {
+	return &PIR{
+		signalPin: rpio.Pin(pinNumber),
+	}
+}
+
+// Read returns the current state of the PIR sensor (true for motion detected, false otherwise)
+func (p *PIR) Read() bool {
+	return p.signalPin.Read() == rpio.High
+}
 
 func main() {
-	// Open and map memory to access gpio, check for errors
+	// Open and map memory to access GPIO, check for errors
 	if err := rpio.Open(); err != nil {
 		fmt.Println("Unable to open GPIO:", err)
 		os.Exit(1)
 	}
 	defer rpio.Close()
 
-	// Set pin to input mode
-	pin := rpio.Pin(sensorPin)
-	pin.Input()
+	// Create a new PIR sensor instance connected to GPIO pin 17
+	pirSensor := NewPIR(17)
+
+	// Set the sensor pin to input mode
+	pirSensor.signalPin.Input()
 
 	fmt.Println("Monitoring for motion...")
 
 	for {
-		// Read pin state
-		if pin.Read() == rpio.High {
+		// Read the sensor state
+		if pirSensor.Read() {
 			fmt.Println("Motion detected!")
 		} else {
 			fmt.Println("No motion.")
 		}
-
-		// Sleep for a while to reduce CPU usage
-		time.Sleep(1 * time.Second)
 	}
 }

2803fb0ba5542ee3e5594ee914324efdc6168f41
mabraham2o24 <mahima200016@gmail.com>
2024-03-28 21:08:49 -0400

    update HVAC.css
    added text fields to all of the different stats for the floors.

--- fab/my-login-app/src/HVAC.js
+++ fab/my-login-app/src/HVAC.js
@@ -28,6 +28,51 @@ const HVAC = () => {
    // States for date and time
    const [currentDate, setCurrentDate] = useState(new Date().toLocaleDateString());
    const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+   const [secondFloorMode, setSecondFloorMode] = useState(deviceData.HVAC.secondFloorMode || 'Heat');
+   const [basementMode, setBasementMode] = useState(deviceData.HVAC.basementMode || 'Heat');
+   const [secondFloorTemp, setSecondFloorTemp] = useState(deviceData.HVAC.secondFloorTemp || '');
+   const [basementTemp, setBasementTemp] = useState(deviceData.HVAC.basementTemp || '');
+   const [secondFloorHumidity, setSecondFloorHumidity] = useState(deviceData.HVAC.secondFloorHumidity || '');
+   const [basementHumidity, setBasementHumidity] = useState(deviceData.HVAC.basementHumidity || '');
+   const [secondFloorFanSpeed, setSecondFloorFanSpeed] = useState(deviceData.HVAC.secondFloorFanSpeed || '');
+   const [basementFanSpeed, setBasementFanSpeed] = useState(deviceData.HVAC.basementFanSpeed || '');
+   const [secondFloorEnergyConsumption, setSecondFloorEnergyConsumption] = useState(deviceData.HVAC.secondFloorEnergyConsumption || '');
+   const [basementEnergyConsumption, setBasementEnergyConsumption] = useState(deviceData.HVAC.basementEnergyConsumption || '');
+   const [secondFloorHVACStatus, setSecondFloorHVACStatus] = useState(deviceData.HVAC.Status);
+  const [basementHVACStatus, setBasementHVACStatus] = useState(deviceData.HVAC.Status);
+
+
+   const updateSecondFloorHVACMode = (newMode) => {
+    setDeviceData(prevDeviceData => ({
+        ...prevDeviceData,
+        HVAC: {
+            ...prevDeviceData.HVAC,
+            secondFloorMode: newMode,
+        },
+    }));
+};
+
+const updateBasementHVACMode = (newMode) => {
+    setDeviceData(prevDeviceData => ({
+        ...prevDeviceData,
+        HVAC: {
+            ...prevDeviceData.HVAC,
+            basementMode: newMode,
+        },
+    }));
+};
+
+const updateDeviceData = (key, value) => {
+  setDeviceData(prevDeviceData => ({
+    ...prevDeviceData,
+    HVAC: {
+      ...prevDeviceData.HVAC,
+      [key]: value,
+    },
+  }));
+};
+
+
  
    useEffect(() => {
        const timer = setInterval(() => {
@@ -44,22 +89,22 @@ const HVAC = () => {
     const [dashboardMessage, setDashboardMessage] = useState('');
     const [accountType ,setAccountType] = useState('')
     
-    const [secondFloorHVACStatus, setSecondFloorHVACStatus] = useState(deviceData.HVAC.Status);
-    const [basementHVACStatus, setBasementHVACStatus] = useState(deviceData.HVAC.Status);
+    
+  
 
    // Toggle function for 2nd floor HVAC status
-  const toggleSecondFloorHVACStatus = () => {
-    setSecondFloorHVACStatus(prevStatus => !prevStatus);
-    // Update the actual device data
-    updateHVACStatus(!secondFloorHVACStatus);
-  };
+   const toggleSecondFloorHVACStatus = () => {
+    const newStatus = !secondFloorHVACStatus;
+    setSecondFloorHVACStatus(newStatus);
+    updateDeviceData('secondFloorStatus', newStatus);
+};
 
-  // Toggle function for basement HVAC status
-  const toggleBasementHVACStatus = () => {
-    setBasementHVACStatus(prevStatus => !prevStatus);
-    // Update the actual device data
-    updateHVACStatus(!basementHVACStatus);
-  };
+const toggleBasementHVACStatus = () => {
+    const newStatus = !basementHVACStatus;
+    setBasementHVACStatus(newStatus);
+    updateDeviceData('basementStatus', newStatus);
+};
+
 
 
     useEffect(() => {
@@ -250,45 +295,114 @@ const HVAC = () => {
       {/* First Column */}
       <div style={{ marginRight: '2rem' }}>
         <h4 style={{textAlign:"center", marginBottom:'20px', color: '#95A4B6'}}>2nd Floor</h4>
-
-        <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-thermometer-half"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Mode</p>
-            <p>{deviceData.HVAC.Mode}</p>
-          </div>
-        </div>
-
-        <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-thermometer-half"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Temperature</p>
-            <p>{deviceData.HVAC.Temperature}°F</p>
-          </div>
-        </div>
         
+        {/* First Column - mode */}
         <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-tint"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Humidity</p>
-            <p>{deviceData.HVAC.Humidity}%</p>
-          </div>
-        </div>
+    <div className="data-icon">
+        <i className="fas fa-thermometer-half"></i>
+    </div>
+    <div className="data-info">
+        <p style={{ color: '#95A4B6' }}>Mode</p>
+        <select
+    value={secondFloorMode}
+    onChange={(e) => {
+        setSecondFloorMode(e.target.value);
+        updateSecondFloorHVACMode(e.target.value);
+    }}
+            style={{ 
+                color: '#95A4B6',
+                backgroundColor: '#08192B',
+                border: 'none',
+                borderRadius: '5px',
+                padding: '5px',
+                zIndex: 10, // Ensure dropdown is clickable
+                position: 'relative' // Required for z-index to work
+            }}
+        >
+            <option value="Heat">Heat</option>
+            <option value="Cool">Cool</option>
+        </select>
+    </div>
+</div>
+        {/* First Column - temperature */}
+           
         <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-fan"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Fan Speed</p>
-            <p>{deviceData.HVAC.FanSpeed}%</p>
-          </div>
-        </div>
+  <div className="data-icon">
+    <i className="fas fa-thermometer-half"></i>
+  </div>
+  <div className="data-info">
+    <p style={{ color: '#95A4B6' }}>Temperature</p>
+    <input 
+      type="number" 
+      value={secondFloorTemp} 
+      onChange={(e) => setSecondFloorTemp(e.target.value)}
+      onBlur={() => updateDeviceData('secondFloorTemp', secondFloorTemp)}
+      style={{
+        zIndex: '1', // Ensure the input is clickable
+        position: 'relative', // Z-index works on positioned elements
+        // Add more styles to fit your design
+      }}
+      disabled={false} // Ensure the input is not disabled
+      readOnly={false} // Ensure the input is not read-only
+    />
+  </div>
+</div>
+{/* ========================================================================= */}
+
+        {/* First Column - humidity */}
+        <div className="data-item">
+  <div className="data-icon">
+    <i className="fas fa-tint"></i>
+  </div>
+  <div className="data-info">
+    <p style={{ color: '#95A4B6' }}>Humidity</p>
+    <input 
+      type="number" 
+      value={secondFloorHumidity} 
+      onChange={(e) => setSecondFloorHumidity(e.target.value)}
+      onBlur={() => updateDeviceData('secondFloorHumidity', secondFloorHumidity)}
+      style={{
+        zIndex: 2, // Ensure the input is clickable
+        position: 'relative', // Z-index works on positioned elements
+        // Add more styles to fit your design
+      }}
+      min="0"
+      max="100"
+      disabled={false} // Ensure the input is not disabled
+      readOnly={false} // Ensure the input is not read-only
+    />
+  </div>
+</div>
+{/* ========================================================================= */}
+
+        {/* First Column - fan speed */}
+        <div className="data-item">
+  <div className="data-icon">
+    <i className="fas fa-fan"></i>
+  </div>
+  <div className="data-info">
+    <p style={{ color: '#95A4B6'}}>Fan Speed</p>
+    <input 
+      type="number" 
+      value={secondFloorFanSpeed} 
+      onChange={(e) => setSecondFloorFanSpeed(e.target.value)}
+      onBlur={() => updateDeviceData('secondFloorFanSpeed', secondFloorFanSpeed)}
+      style={{
+        zIndex: 2, // Ensure the input is clickable
+        position: 'relative', // Z-index works on positioned elements
+        // Add more styles to fit your design
+      }}
+      min="0"
+      max="100" // Assuming fan speed is a percentage from 0 to 100
+      disabled={false} // Ensure the input is not disabled
+      readOnly={false} // Ensure the input is not read-only
+    />
+  </div>
+</div>
+
+{/* ========================================================================= */}
+
+        {/* First Column - status */}
         <div className="data-item">
           <div className="data-icon">
             <i className={deviceData.HVAC.Status ? "fas fa-power-on" : "fas fa-power-off"}></i>
@@ -300,66 +414,154 @@ const HVAC = () => {
             <label className="toggle" style={{ display: 'block', margin: 'auto' }}>
               <input
                 type="checkbox"
-                checked={deviceData.HVAC.Status}
+                checked={secondFloorHVACStatus}
                 onChange={toggleSecondFloorHVACStatus}
               />
-              <span className="slider" style={{ background: deviceData.HVAC.Status ? '#50BCC0' : 'grey' }}></span>
+              <span className="slider" style={{ background: secondFloorHVACStatus ? '#50BCC0' : 'grey' }}></span>
             </label>
-          </div>
-        </div>
-        <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-bolt"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Energy Consumption</p>
-            <p>{deviceData.HVAC.EnergyConsumption}KW</p>
-          </div>
-        </div>
-      </div>
 
-      {/* Second Column (Duplicated) */}
+          </div>
+        </div>
+{/* ========================================================================= */}
+
+        {/* First Column - energy consumption */}
+        <div className="data-item">
+  <div className="data-icon">
+    <i className="fas fa-bolt"></i>
+  </div>
+  <div className="data-info">
+    <p style={{ color: '#95A4B6'}}>Energy Consumption</p>
+    <input 
+      type="number" 
+      value={secondFloorEnergyConsumption} 
+      onChange={(e) => setSecondFloorEnergyConsumption(e.target.value)}
+      onBlur={() => updateDeviceData('secondFloorEnergyConsumption', secondFloorEnergyConsumption)}
+      style={{
+        zIndex: 2, // Make sure the input is above other elements
+        position: 'relative', // Necessary for z-index to take effect
+        // Define additional styles here
+      }}
+      min="0" // Set minimum value as needed
+      // You can also add step attribute to control the valid steps
+    />
+  </div>
+</div>
+
+</div>
+
+{/* ============================================================== */}
+
+      {/* Second Column (Basement Floor) */}
+      {/* Second Column - Mode */}
       <div>
       <h4 style={{textAlign:"center", marginBottom:'20px', color: '#95A4B6'}}>Basement</h4>
 
       <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-thermometer-half"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Mode</p>
-            <p>{deviceData.HVAC.Mode}</p>
-          </div>
-        </div>
+    <div className="data-icon">
+        <i className="fas fa-thermometer-half"></i>
+    </div>
+    <div className="data-info">
+        <p style={{ color: '#95A4B6' }}>Mode</p>
+        <select
+    value={basementMode}
+    onChange={(e) => {
+        setBasementMode(e.target.value);
+        updateBasementHVACMode(e.target.value);
+    }}
+            style={{ 
+                color: '#95A4B6',
+                backgroundColor: '#08192B',
+                border: 'none',
+                borderRadius: '5px',
+                padding: '5px',
+                zIndex: 10, // Ensure dropdown is clickable
+                position: 'relative' // Required for z-index to work
+            }}
+        >
+            <option value="Heat">Heat</option>
+            <option value="Cool">Cool</option>
+        </select>
+    </div>
+</div>
+{/* ============================================================== */}
 
-        <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-thermometer-half"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Temperature</p>
-            <p>{deviceData.HVAC.Temperature}°F</p>
-          </div>
-        </div>
-        
-        <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-tint"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Humidity</p>
-            <p>{deviceData.HVAC.Humidity}%</p>
-          </div>
-        </div>
-        <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-fan"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Fan Speed</p>
-            <p>{deviceData.HVAC.FanSpeed}%</p>
-          </div>
-        </div>
+{/* Second Column - Temperature */}
+<div className="data-item">
+  <div className="data-icon">
+    <i className="fas fa-thermometer-half"></i>
+  </div>
+  <div className="data-info">
+    <p style={{ color: '#95A4B6' }}>Temperature</p>
+    <input 
+      type="number" 
+      value={basementTemp} 
+      onChange={(e) => setBasementTemp(e.target.value)}
+      onBlur={() => updateDeviceData('basementTemp', basementTemp)}
+      style={{
+        zIndex: '1', // Ensure the input is clickable
+        position: 'relative', // Z-index works on positioned elements
+        // Add more styles to fit your design
+      }}
+      disabled={false} // Ensure the input is not disabled
+      readOnly={false} // Ensure the input is not read-only
+    />
+  </div>
+</div>
+{/* ============================================================== */}
+
+  {/* Second Column - Humidity */}   
+<div className="data-item">
+  <div className="data-icon">
+    <i className="fas fa-tint"></i>
+  </div>
+  <div className="data-info">
+    <p style={{ color: '#95A4B6' }}>Humidity</p>
+    <input 
+      type="number" 
+      value={basementHumidity} 
+      onChange={(e) => setBasementHumidity(e.target.value)}
+      onBlur={() => updateDeviceData('basementHumidity', basementHumidity)}
+      style={{
+        zIndex: 2, // Ensure the input is clickable
+        position: 'relative', // Z-index works on positioned elements
+        // Add more styles to fit your design
+      }}
+      min="0"
+      max="100"
+      disabled={false} // Ensure the input is not disabled
+      readOnly={false} // Ensure the input is not read-only
+    />
+  </div>
+</div>
+{/* ============================================================== */}
+
+{/* Second Column - Fan Speed */}
+<div className="data-item">
+  <div className="data-icon">
+    <i className="fas fa-fan"></i>
+  </div>
+  <div className="data-info">
+    <p style={{ color: '#95A4B6'}}>Fan Speed</p>
+    <input 
+      type="number" 
+      value={basementFanSpeed} 
+      onChange={(e) => setBasementFanSpeed(e.target.value)}
+      onBlur={() => updateDeviceData('basementFanSpeed', basementFanSpeed)}
+      style={{
+        zIndex: 2, // Ensure the input is clickable
+        position: 'relative', // Z-index works on positioned elements
+        // Add more styles to fit your design
+      }}
+      min="0"
+      max="100" // Assuming fan speed is a percentage from 0 to 100
+      disabled={false} // Ensure the input is not disabled
+      readOnly={false} // Ensure the input is not read-only
+    />
+  </div>
+</div>
+{/* ============================================================== */}
+
+{/* Second Column - Status */}
         <div className="data-item">
           <div className="data-icon">
             <i className={deviceData.HVAC.Status ? "fas fa-power-on" : "fas fa-power-off"}></i>
@@ -371,22 +573,39 @@ const HVAC = () => {
             <label className="toggle" style={{ display: 'block', margin: 'auto' }}>
               <input
                 type="checkbox"
-                checked={deviceData.HVAC.Status}
+                checked={basementHVACStatus}
                 onChange={toggleBasementHVACStatus}
               />
-              <span className="slider" style={{ background: deviceData.HVAC.Status ? '#50BCC0' : 'grey' }}></span>
+              <span className="slider" style={{ background: basementHVACStatus ? '#50BCC0' : 'grey' }}></span>
             </label>
+
           </div>
         </div>
+{/* ============================================================== */}
+
+        {/* Second Column - Energy Consumption */}
         <div className="data-item">
-          <div className="data-icon">
-            <i className="fas fa-bolt"></i>
-          </div>
-          <div className="data-info">
-            <p style={{ color: '#95A4B6'}}>Energy Consumption</p>
-            <p>{deviceData.HVAC.EnergyConsumption}KW</p>
-          </div>
-        </div>
+  <div className="data-icon">
+    <i className="fas fa-bolt"></i>
+  </div>
+  <div className="data-info">
+    <p style={{ color: '#95A4B6'}}>Energy Consumption</p>
+    <input 
+      type="number" 
+      value={basementEnergyConsumption} 
+      onChange={(e) => setBasementEnergyConsumption(e.target.value)}
+      onBlur={() => updateDeviceData('basementEnergyConsumption', basementEnergyConsumption)}
+      style={{
+        zIndex: 2, // Make sure the input is above other elements
+        position: 'relative', // Necessary for z-index to take effect
+        // Define additional styles here
+      }}
+      min="0" // Set minimum value as needed
+      // You can also add step attribute to control the valid steps
+    />
+  </div>
+</div>
+
       </div>
     </div>
   )}

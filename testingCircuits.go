f7d9855986715f0b8731efb97dc63128506f64b8
CBussom <cameron.bussom@gmail.com>
2024-03-28 22:36:36 -0400

    wrapper

--- testingCircuits.go
+++ testingCircuits.go
@@ -1,82 +1,148 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	"log"
+	"periph.io/x/periph/host"
 	"time"
 
-	"github.com/stianeikeland/go-rpio/v4"
+	"periph.io/x/conn/v3/gpio"
+	"periph.io/x/conn/v3/gpio/gpioreg"
 )
 
 const (
-	dinPinNumber = 9  // GPIO pin for DIN (MOSI)
-	csPinNumber  = 4  // GPIO pin for CS
-	clkPinNumber = 10 // GPIO pin for CLK
+	csPin  = "GPIO4"
+	dinPin = "GPIO9"
+	clkPin = "GPIO10"
+)
+
+var (
+	cs  gpio.PinOut
+	din gpio.PinOut
+	clk gpio.PinOut
 )
 
 func main() {
-	if err := rpio.Open(); err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to open GPIO: %v\n", err)
-		os.Exit(1)
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
 	}
-	defer rpio.Close()
 
-	// Set up the pins
-	dinPin := rpio.Pin(dinPinNumber)
-	csPin := rpio.Pin(csPinNumber)
-	clkPin := rpio.Pin(clkPinNumber)
+	cs = gpioreg.ByName(csPin)
+	din = gpioreg.ByName(dinPin)
+	clk = gpioreg.ByName(clkPin)
 
-	dinPin.Output()
-	csPin.Output()
-	clkPin.Output()
-
-	// Initialize the LED matrix
-	initializeMatrix(dinPin, csPin, clkPin)
-
-	// Turn on all LEDs
-	for row := 0; row < 8; row++ {
-		sendData(csPin, dinPin, clkPin, byte(row+1), 0xFF)
+	if cs == nil || din == nil || clk == nil {
+		log.Fatal("Failed to find GPIO pins")
 	}
-	time.Sleep(5 * time.Second)
 
-	// Turn off all LEDs
-	for row := 0; row < 8; row++ {
-		sendData(csPin, dinPin, clkPin, byte(row+1), 0x00)
+	if err := cs.Out(gpio.Low); err != nil {
+		log.Fatal(err)
+	}
+	if err := din.Out(gpio.Low); err != nil {
+		log.Fatal(err)
+	}
+	if err := clk.Out(gpio.Low); err != nil {
+		log.Fatal(err)
+	}
+
+	initializeMatrix()
+	clearMatrix()
+	drawA()
+	time.Sleep(30 * time.Second)
+	clearMatrix()
+}
+
+func sendByte(data byte) {
+	for i := 0; i < 8; i++ {
+		din.Out(gpio.Level(data&0x80 != 0))
+		clk.Out(gpio.High)
+		time.Sleep(100 * time.Microsecond)
+		clk.Out(gpio.Low)
+		data <<= 1
 	}
 }
 
-// Initialize the LED matrix
-func initializeMatrix(dinPin, csPin, clkPin rpio.Pin) {
-	// Set the scan-limit to show all 8 digits
-	sendData(csPin, dinPin, clkPin, 0x0B, 0x07)
-
-	// Use normal operation mode (not test mode)
-	sendData(csPin, dinPin, clkPin, 0x0F, 0x00)
-
-	// Set the intensity (brightness) of the display
-	sendData(csPin, dinPin, clkPin, 0x0A, 0x0F)
-
-	// Turn on the display
-	sendData(csPin, dinPin, clkPin, 0x0C, 0x01)
+func sendCommand(register, data byte) {
+	cs.Out(gpio.Low)
+	sendByte(register)
+	sendByte(data)
+	cs.Out(gpio.High)
 }
 
-// Send data to the LED matrix
-func sendData(csPin, dinPin, clkPin rpio.Pin, address, data byte) {
-	csPin.Low()
-	sendByte(dinPin, clkPin, address)
-	sendByte(dinPin, clkPin, data)
-	csPin.High()
+func initializeMatrix() {
+	sendCommand(0x0B, 0x07)
+	sendCommand(0x0A, 0x04)
+	sendCommand(0x0C, 0x01)
+	sendCommand(0x0F, 0x00)
 }
 
-// Send a single byte of data
-func sendByte(dinPin, clkPin rpio.Pin, data byte) {
-	for i := 7; i >= 0; i-- {
-		clkPin.Low()
-		if (data & (1 << i)) != 0 {
-			dinPin.High()
-		} else {
-			dinPin.Low()
-		}
-		clkPin.High()
+func clearMatrix() {
+	for i := 1; i <= 8; i++ {
+		sendCommand(byte(i), 0x00)
+	}
+}
+
+func drawA() {
+	a := []byte{
+		0b00011000,
+		0b00111100,
+		0b01100110,
+		0b01100110,
+		0b01111110,
+		0b01100110,
+		0b01100110,
+		0b01100110,
+	}
+	for row, pattern := range a {
+		sendCommand(byte(row+1), pattern)
+	}
+}
+func drawLightbulb() {
+	clearMatrix()
+	lightbulbPattern := []byte{
+		0b00111100,
+		0b01111110,
+		0b01111110,
+		0b01111110,
+		0b01111110,
+		0b00111100,
+		0b00011000,
+		0b00011000,
+	}
+	for row, pattern := range lightbulbPattern {
+		sendCommand(byte(row+1), pattern)
+	}
+}
+
+func drawLock() {
+	clearMatrix()
+	lockPattern := []byte{
+		0b00111100,
+		0b00100100,
+		0b01100110,
+		0b01111110,
+		0b01111110,
+		0b01111110,
+		0b01111110,
+		0b01111110,
+	}
+	for row, pattern := range lockPattern {
+		sendCommand(byte(row+1), pattern)
+	}
+}
+
+func drawH() {
+	clearMatrix()
+	hPattern := []byte{
+		0b10000001,
+		0b10000001,
+		0b10000001,
+		0b11111111,
+		0b10000001,
+		0b10000001,
+		0b10000001,
+		0b10000001,
+	}
+	for row, pattern := range hPattern {
+		sendCommand(byte(row+1), pattern)
 	}
 }

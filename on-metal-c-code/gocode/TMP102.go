36b5476cdcd35bafd7d9c26ac7588499186f3a15
CBussom <cameron.bussom@gmail.com>
2024-04-01 18:08:17 -0400

    testing

--- on-metal-c-code/gocode/TMP102.go
+++ on-metal-c-code/gocode/TMP102.go
@@ -3,10 +3,6 @@ package gocode
 import (
 	"fmt"
 	"log"
-
-	"periph.io/x/periph/conn/i2c"
-	"periph.io/x/periph/conn/i2c/i2creg"
-	"periph.io/x/periph/host"
 )
 
 // TMP102 represents a TMP102 temperature sensor.

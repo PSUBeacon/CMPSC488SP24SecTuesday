99744e0d6ccc0e57bdb267346eb5ce63066d5a64
CBussom <52250345+CBussom@users.noreply.github.com>
2024-04-02 13:01:10 -0400

    added locking mechanism code

---
+++ on-metal-c-code/gocode/Servo.go
@@ -0,0 +1,30 @@
+package gocode
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/stianeikeland/go-rpio/v4"
+)
+
+func TurnServoTo90Degrees() {
+	// Open and map memory to access gpio, check for errors
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	// Define the GPIO pin
+	servoPin := rpio.Pin(18) // Use the correct pin for your setup
+	servoPin.Mode(rpio.Pwm)  // Set the pin to PWM mode
+
+	// Manually control PWM for servo
+	const dutyCycle = 150               // Adjust this value for 90 degrees based on your servo
+	servoPin.Freq(50 * 1000)            // Set frequency to 50Hz
+	servoPin.DutyCycle(dutyCycle, 1000) // Set duty cycle
+
+	// Wait for the servo to move
+	time.Sleep(1 * time.Second)
+}

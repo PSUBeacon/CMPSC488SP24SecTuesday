7bdc12c9ed934ce21d13223c6e2cbc52fdec6590
CBussom <cameron.bussom@gmail.com>
2024-04-09 19:06:14 -0400

    testing fan

--- testingFan.go
+++ testingFan.go
@@ -43,6 +43,12 @@ import (
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
+const (
+	lowSpeed    = 10
+	mediumSpeed = 50
+	highSpeed   = 90
+)
+
 func main() {
 	// Open and map memory to access GPIO, check for errors
 	if err := rpio.Open(); err != nil {
@@ -51,28 +57,38 @@ func main() {
 	}
 	defer rpio.Close()
 
-	// Set pin to PWM mode
+	// Set pin to output mode
 	pin := rpio.Pin(18)
-	pin.Mode(rpio.Pwm)
-	pin.Freq(19200000) // Set PWM frequency
-	pin.DutyCycle(0, 1)
+	pin.Output()
 
 	// Set the fan to low speed
-	pin.DutyCycle(1920000, 19200000) // 10% duty cycle
+	setFanSpeed(pin, lowSpeed)
 	fmt.Println("Fan LOW")
 	time.Sleep(2 * time.Second)
 
 	// Set the fan to medium speed
-	pin.DutyCycle(9600000, 19200000) // 50% duty cycle
+	setFanSpeed(pin, mediumSpeed)
 	fmt.Println("Fan MEDIUM")
 	time.Sleep(2 * time.Second)
 
 	// Set the fan to high speed
-	pin.DutyCycle(17280000, 19200000) // 90% duty cycle
+	setFanSpeed(pin, highSpeed)
 	fmt.Println("Fan HIGH")
 	time.Sleep(2 * time.Second)
 
 	// Turn the fan off
-	pin.DutyCycle(0, 1)
+	pin.Low()
 	fmt.Println("Fan OFF")
+}
+
+// setFanSpeed controls the fan speed using software PWM
+func setFanSpeed(pin rpio.Pin, speed int) {
+	onTime := time.Duration(speed) * time.Millisecond
+	offTime := 100*time.Millisecond - onTime
+	for i := 0; i < 50; i++ { // Run PWM for a short period
+		pin.High()
+		time.Sleep(onTime)
+		pin.Low()
+		time.Sleep(offTime)
+	}
 }

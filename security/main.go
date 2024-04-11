fef647e24b0b71deeb695e537540d73e8e66f537
CBussom <52250345+CBussom@users.noreply.github.com>
2024-04-11 08:14:10 -0400

    testing

--- security/main.go
+++ security/main.go
@@ -2,7 +2,9 @@ package security
 
 import (
 	"CMPSC488SP24SecTuesday/on-metal-c-code/gocode"
+	"encoding/json"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -69,19 +71,25 @@ func HandleMotionDetection() {
 	}
 }
 
+type System struct {
+	UUID         string `json:"UUID"`
+	Status       string `json:"Status"`
+	SensorStatus string `json:"SensorStatus"`
+}
+
 func DisplayLCDSecurity(status string, motionStatus string) {
-	defaults := DefaultSecurity{
-		Status:       "Armed",
-		MotionStatus: "off",
+	jsonSecurityData, err := os.ReadFile("security/Keys.json")
+	if err != nil {
+		fmt.Println("Error reading key data:", err)
+		return
+	}
+	//fmt.Println("Thermostat data, ", jsonThermData)
+	// Unmarshal the JSON data into a Thermostat struct
+	var securitySystem System
+	if err := json.Unmarshal(jsonSecurityData, &securitySystem); err != nil {
+		fmt.Println("Error unmarshalling security data:", err)
+		return
 	}
 
-	if status == "" {
-		status = defaults.Status
-	}
-	if motionStatus == "" {
-		motionStatus = defaults.MotionStatus
-	}
-	//key := gocode.InitKeypad()
-
-	gocode.WriteLCD("Stat:" + status + " Motion:" + motionStatus)
+	gocode.WriteLCD("Stat:" + securitySystem.Status + " Motion:" + securitySystem.SensorStatus)
 }

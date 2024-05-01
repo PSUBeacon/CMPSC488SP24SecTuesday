98ad2901a0bec32a756f88fee224f3a64a72308e
CBussom <cameron.bussom@gmail.com>
2024-05-01 00:23:10 -0400

    moving on to the servo pain

---
+++ testingServo.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stianeikeland/go-rpio/v4"
+	"os"
+	"time"
+)
+
+func main() {
+	// Open and map memory to access gpio, check for errors
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer rpio.Close()
+
+	// Define the GPIO pin
+	servoPin := rpio.Pin(8) // Use the correct pin for your setup
+	servoPin.Mode(rpio.Pwm) // Set the pin to PWM mode
+
+	// Manually control PWM for servo
+	const dutyCycle = 150               // Adjust this value for 90 degrees based on your servo
+	servoPin.Freq(50 * 1000)            // Set frequency to 50Hz
+	servoPin.DutyCycle(dutyCycle, 1000) // Set duty cycle
+
+	// Wait for the servo to move
+	time.Sleep(1 * time.Second)
+}

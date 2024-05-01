7f188b0100be75b1daa4989cc7fbfc1e1c459fed
CBussom <52250345+CBussom@users.noreply.github.com>
2024-05-01 19:51:36 -0400

    testing alarm

--- on-metal-c-code/gocode/PIR.go
+++ on-metal-c-code/gocode/PIR.go
@@ -6,15 +6,15 @@ import (
 
 // CheckForMotion initializes the GPIO then checks for motion
 func CheckForMotion(pinNum uint8) (bool, error) {
-	if err := rpio.Open(); err != nil {
-		return false, err
-	}
-	defer func() {
-		err := rpio.Close()
-		if err != nil {
-
-		}
-	}()
+	//if err := rpio.Open(); err != nil {
+	//	return false, err
+	//}
+	//defer func() {
+	//	err := rpio.Close()
+	//	if err != nil {
+	//
+	//	}
+	//}()
 
 	pin := rpio.Pin(pinNum)
 	pin.Input()
